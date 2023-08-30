640c634796973eff5582d86ee0201a5763d97398
hieuchunhat <vongochieu221998@gmail.com>
2023-08-30 10:29:05 +0700

    update

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -1,10 +1,34 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import * as TodoAccess from './todosAcess'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
-import { createLogger } from '../utils/logger'
-import * as uuid from 'uuid'
-import * as createError from 'http-errors'
+import { v4 as uuidv4 } from 'uuid';
+import { TodoUpdate } from '../models/TodoUpdate';
+
+const bucketName = process.env.ATTACHMENT_S3_BUCKET;
 
 // TODO: Implement businessLogic
+export async function getAllTodo(userId: string): Promise<TodoItem[]> {
+    return TodoAccess.findAllTodoByUserId(userId);
+}
+
+export async function createTodo(userId: string, request: CreateTodoRequest): Promise<TodoItem> {
+    var todoId = uuidv4();
+    var imageUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`;
+    return TodoAccess.create({
+        userId: userId,
+        todoId: todoId,
+        createdAt: new Date().getTime().toString(),
+        attachmentUrl: imageUrl,
+        done: false,
+        ...request
+    });
+}
+
+export async function updateTodo(userId: string, todoId: string, request: UpdateTodoRequest): Promise<TodoUpdate> {
+    return TodoAccess.update({ ...request }, userId, todoId);
+}
+
+export async function deleteTodo(userId: string, todoId: string): Promise<string> {
+    return TodoAccess.remove(userId, todoId);
+}
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -4,16 +4,24 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { uploadImage } from '../../helpers/attachmentUtils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId;
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    const url = uploadImage(todoId);
 
-    return undefined
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        uploadUrl: url
+      })
+    };
   }
 )
 
